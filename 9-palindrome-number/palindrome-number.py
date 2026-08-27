f213e502ea4b9a404debec866330241a87e509b2
Dileep Gopireddy <gopireddydileep989@gmail.com>
2026-08-27 10:49:57 +0530

    i updated the inputnum to input_num

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -2,7 +2,7 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x<0:
             return False
-        inputnum = x
+        input_num = x
         newnum = 0
         while x>0:
             newnum = newnum*10+x%10
